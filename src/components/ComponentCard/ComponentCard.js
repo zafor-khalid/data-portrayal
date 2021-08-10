e804114158d55ce74506711240bfd8265fa977b0
Zafor Khalid <zaforkhalid@gmail.com>
2021-08-10 16:47:37 +0600

    animation, effect added

--- src/components/ComponentCard/ComponentCard.js
+++ src/components/ComponentCard/ComponentCard.js
@@ -3,6 +3,29 @@ import ComponentCardStyless from '../../../styles/ComponentCard.module.css';
 import { dataContext } from '../Contexts/Contexts';
 import { stylesContext, languageContext } from '../Contexts/Contexts';
 import React, { useContext, useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { motion } from "framer-motion"
+
+const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+        opacity: 1,
+        scale: 1,
+        transition: {
+            delayChildren: 1,
+            staggerChildren: .6
+        }
+    }
+}
+
+const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+        y: 0,
+        opacity: 1
+    }
+}
 
 const ComponentCard = () => {
     const [pageData, setPageData] = useContext(dataContext);
@@ -16,10 +39,10 @@ const ComponentCard = () => {
         color: styles?.style?.["card-text-color"],
         fontFamily: styles?.style?.["card-font"],
         width: '30vh',
-        minWidth:'25vh'
-       
+        minWidth: '25vh',
+
     }
-  
+
     // console.log(cardStyle);
 
     const handleDetail = (detail, status) => {
@@ -41,30 +64,64 @@ const ComponentCard = () => {
 
 
     return (
-        <div className="d-flex m-5 flex-wrap justify-content-center align-items-center" >
+
+        <div className="mt-5 mb-5">
+
             {
                 !componentStatus ?
-                    pageData.cards?.map((data, idx) => (
-                        <Card key={idx} style={cardStyle} className="m-3 text-center">
-                            <Card.Body>
-                                <Card.Title>{language=='en'? data.title_en + " $" : data.title_bn +" "}{language=='en' ? data.value_en : data.value_bn + "৳"}</Card.Title>
-                                <Button onClick={() => handleDetail(data.detail, true)} variant="dark">See Details</Button>
 
-                            </Card.Body>
-                        </Card>
-                    ))
-                    : categoryPageData?.cards?.map((data, idx) => (
-                        <Card key={idx} style={cardStyle} className="m-2 text-center">
-                            <Card.Img variant="top" src={data.photo} />
-                            <Card.Body>
-                                <Card.Title>{language=='en'? data.title_en + " $" : data.title_bn +" "}{language=='en' ? data.value_en : data.value_bn + "৳"}</Card.Title>
-                                {/* <Button onClick={() => handleReturn(false)} variant="primary">Return</Button> */}
+                    <motion.div className="container"
+                        variants={container}
+                        initial="hidden"
+                        animate="visible"
+                        >
+                        <div>
+                            {
+                                pageData.cards?.map((data, idx) => (
+                                    <Card key={idx} style={cardStyle} className="mt-5  text-center customCard m-auto " >
+                                        <Card.Body>
+                                            <Card.Title>{language == 'en' ? data.title_en + " $" : data.title_bn + " "}{language == 'en' ? data.value_en : data.value_bn + "৳"}</Card.Title>
+                                            <Button onClick={() => handleDetail(data.detail, true)} className='customButton' size='sm'>See Details<FontAwesomeIcon icon={faArrowRight} style={{ height: '12px', width: '25px' }} /></Button>
 
-                            </Card.Body>
-                        </Card>
-                    ))
+                                        </Card.Body>
+                                    </Card>
+                                ))
+                            }
+                        </div>
+                    </motion.div>
+                    : <>
+
+
+                        <Button className="customButton" onClick={() => handleReturn(false)} variant="primary" style={{ postion: 'absolute' }}><FontAwesomeIcon icon={faArrowLeft} style={{ height: '12px', width: '25px' }} />Return</Button>
+
+                        <div className=' d-flex flex-wrap justify-content-center align-items-center'>
+
+                            {
+                                categoryPageData?.cards?.map((data, idx) => (
+                                    <motion.div key={idx}  className=""
+                                        variants={container}
+                                        initial="hidden"
+                                        animate="visible"
+                                        whileHover={{scale: 1.03}}
+                                        >
+
+                                        <Card style={cardStyle} className="m-2 text-center cardView">
+                                            <Card.Img variant="top" src={data.photo} />
+                                            <Card.Body className='d-flex align-items-center justify-content-center'>
+                                                <Card.Title>{language == 'en' ? data.title_en + " $" : data.title_bn + " "}{language == 'en' ? data.value_en : data.value_bn + "৳"}</Card.Title>
+
+                                            </Card.Body>
+                                        </Card>
+                                    </motion.div>
+                                ))
+                            }
+
+                        </div>
+
+                    </>
             }
         </div>
+
     );
 };
 
--- src/components/Shared/NavigationBar/NavigationBar.js
+++ src/components/Shared/NavigationBar/NavigationBar.js
@@ -2,11 +2,12 @@ import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import styles from '../../../../styles/NavigationBar.module.css'
 import React, { useContext, useEffect, useState } from 'react';
 import { languageContext } from '../../Contexts/Contexts'
+
 const NavigationBar = () => {
     const [language, setLanguage] = useContext(languageContext);
     const handleLanguage = (status) => {
         setLanguage(status);
-      
+
     }
     console.log(language);
     return (
@@ -14,16 +15,19 @@ const NavigationBar = () => {
             <Navbar bg='white' expand="lg" sticky='top' className={styles.navigationBar}>
                 <Container>
                     <Navbar.Brand href="/" style={{ fontWeight: 1000, fontSize: '1.9rem' }}>Data <span style={{ color: '#0F52BA' }}>Portrayal</span></Navbar.Brand>
-                    <Nav className="ms-auto" >
-                        <NavDropdown title="Language" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="" onClick={() => handleLanguage('en')}>en</NavDropdown.Item>
-                            <NavDropdown.Item href="" onClick={() => handleLanguage('bn')}>bn</NavDropdown.Item>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" style={{border:'none', outline: 'none'}} />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto" >
+                            <NavDropdown title={language == 'en' ? "Language" : "ভাষা"} id="basic-nav-dropdown" size="sm" >
+                                <NavDropdown.Item href="" onClick={() => handleLanguage('en')}>English</NavDropdown.Item>
+                                <NavDropdown.Item href="" onClick={() => handleLanguage('bn')}>Bangla</NavDropdown.Item>
 
-                        </NavDropdown>
-                    </Nav>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
-
+                        
         </>
     );
 };
